5f95f7f7235d108a1acae65803781c58df894bbb
dingyx <dyx1222@gmail.com>
2017-10-25 22:51:36 +0800

    add photos

--- tool.py
+++ tool.py
@@ -137,7 +137,7 @@ def cut_photo():
     ----------
     调用Graphics类中的裁剪算法，将src_dir目录下的文件进行裁剪（裁剪成正方形）
     """
-    src_dir, des_dir = "photo_resource/mini_photos/", "photo_resource/mini_photos/"
+    src_dir, des_dir = "photo_resource/photos/", "photo_resource/photos/"
     if directory_exists(src_dir):
         if not directory_exists(src_dir):
             make_directory(src_dir)
@@ -169,11 +169,9 @@ def git_operation():
     os.system('git push origin master')
 
 if __name__ == "__main__":
-	compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
 	cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
+	compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
 	git_operation()    # 提交到github仓库
 	handle_photo()     # 将文件处理成json格式，存到博客仓库中
-    
-    
     
     
